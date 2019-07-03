fd362da78ffe25cdd8f5e1263c127e14ff6d09ef
Sheela123sweety <52439344+Sheela123sweety@users.noreply.github.com>
2019-07-03 18:26:49 +0530

    Add files via upload

--- Stdhome.php
+++ Stdhome.php
@@ -1,8 +1,22 @@
+<?php 
+include('dbconnect.php');
+
+if(isset($_POST['logout'])){
+
+session_destroy();
+session_unset();
+header("refresh:0;url='login.php'");
+
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <body>
 
-<h1>hi good evening</h1>
+<h1> hi good evening <?php echo $_SESSION['username'] ?> </h1><br>
+<button type="submit" name="logout"><b>Logout</b></button><br>
+
 
 </body>
 </html>
